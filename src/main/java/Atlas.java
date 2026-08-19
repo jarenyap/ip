c61b9f99337a79ece1cd18d60a05d9cbff40132c
jarenyap <227606516+jarenyap@users.noreply.github.com>
2026-08-19 21:50:28 +0800

    Add Level-4: todos, deadlines, events

--- src/main/java/Atlas.java
+++ src/main/java/Atlas.java
@@ -13,6 +13,27 @@ public class Atlas {
         System.out.println("╰" + border + "╯");
     }
 
+    /**
+     * Parses a todo/deadline/event command line into a Task.
+     * Lines without a command prefix are treated as plain todos.
+     */
+    static Task parseTask(String line) {
+        if (line.startsWith("todo ")) {
+            return new Task("T", line.substring(5));
+        }
+        if (line.startsWith("deadline ")) {
+            int byPos = line.indexOf(" /by ");
+            return new Task("D", line.substring(9, byPos), line.substring(byPos + 5));
+        }
+        if (line.startsWith("event ")) {
+            int fromPos = line.indexOf(" /from ");
+            int toPos = line.indexOf(" /to ", fromPos);
+            return new Task("E", line.substring(6, fromPos),
+                    line.substring(fromPos + 7, toPos), line.substring(toPos + 5));
+        }
+        return new Task("T", line);
+    }
+
     public static void main(String[] args) {
         String banner = "     _  _____ _        _    ____\n"
                 + "    / \\|_   _| |      / \\  / ___|\n"
@@ -36,24 +57,26 @@ public class Atlas {
                 } else {
                     speak("Here are the tasks in your list:");
                     for (int i = 0; i < taskCount; i++) {
-                        System.out.println((i + 1) + ".[" + tasks[i].getStatusIcon() + "] " + tasks[i].description);
+                        System.out.println((i + 1) + "." + tasks[i]);
                     }
                 }
             } else if (line.startsWith("mark ")) {
                 int index = Integer.parseInt(line.substring(5)) - 1;
                 tasks[index].markAsDone();
                 speak("Nice! I've marked this task as done:");
-                speak("  [" + tasks[index].getStatusIcon() + "] " + tasks[index].description);
+                speak("  " + tasks[index]);
             } else if (line.startsWith("unmark ")) {
                 int index = Integer.parseInt(line.substring(7)) - 1;
                 tasks[index].markAsNotDone();
                 speak("OK, I've marked this task as not done yet:");
-                speak("  [" + tasks[index].getStatusIcon() + "] " + tasks[index].description);
+                speak("  " + tasks[index]);
             } else {
-                tasks[taskCount] = new Task(line);
+                Task t = parseTask(line);
+                tasks[taskCount] = t;
                 taskCount++;
-                speak("Got it. I've added: " + line);
-                speak("You now have " + taskCount + " task" + (taskCount == 1 ? "" : "s") + ".");
+                speak("Got it. I've added this task:");
+                speak("  " + t);
+                speak("Now you have " + taskCount + " task" + (taskCount == 1 ? "" : "s") + " in the list.");
             }
             line = in.nextLine();
         }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,10 +1,30 @@
 public class Task {
     protected String description;
     protected boolean isDone;
+    protected String type;   // "T", "D", or "E"
+    protected String by;     // deadline due time; null if not a deadline
+    protected String from;   // event start; null if not an event
+    protected String to;     // event end; null if not an event
 
-    public Task(String description) {
+    public Task(String type, String description) {
+        this.type = type;
         this.description = description;
         this.isDone = false;
+    }
+
+    public Task(String type, String description, String by) {
+        this.type = type;
+        this.description = description;
+        this.isDone = false;
+        this.by = by;
+    }
+
+    public Task(String type, String description, String from, String to) {
+        this.type = type;
+        this.description = description;
+        this.isDone = false;
+        this.from = from;
+        this.to = to;
     }
 
     public String getStatusIcon() {
@@ -17,5 +37,16 @@ public class Task {
 
     public void markAsNotDone() {
         this.isDone = false;
+    }
+
+    public String toString() {
+        String result = "[" + type + "][" + getStatusIcon() + "] " + description;
+        if (by != null) {
+            result += " (by: " + by + ")";
+        }
+        if (from != null) {
+            result += " (from: " + from + " to: " + to + ")";
+        }
+        return result;
     }
 }
